54727d6596802d097b67601b21ec9f2acbb87084
jongpilbest <daum091409@naver.com>
2026-03-18 15:23:26 +0900

    "docker error"

--- src/main/java/com/example/demo/config/WebConfig.java
+++ src/main/java/com/example/demo/config/WebConfig.java
@@ -11,7 +11,6 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins(
-                        "http://localhost:5173",
                         "https://safesignal.cloud",
                         "https://www.safesignal.cloud"
                 )
