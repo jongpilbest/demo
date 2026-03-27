8ee298f17bfdcc09d65a9ca693f8fd12a088a3d9
jongpilbest <daum091409@naver.com>
2026-03-27 11:31:02 +0900

    yes

--- src/main/java/com/example/demo/dto/LoginRequest.java
+++ src/main/java/com/example/demo/dto/LoginRequest.java
@@ -1,13 +1,28 @@
 package com.example.demo.dto;
 
+import com.example.demo.entity.Member;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
+import lombok.NoArgsConstructor;
 
 @Getter
-@Setter
-public class LoginRequest {
-
+@Builder // ◀ 빌더 사용 선언
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserInformation {
+    private Long id;
     private String username;
-    private String password;
+    private String name;
+    private String role; // ◀ 객체가 아니라 문자열(String)로 선언!
 
+    // Member 엔티티를 받아서 DTO로 변환하는 static 메서드를 만들면 편해요
+    public static UserInformation from(Member member) {
+        return UserInformation.builder()
+                .id(member.getId())
+                .username(member.getUsername())
+                .name(member.getName())
+                .role(member.getRole().getRoleName()) // ◀ 여기서 문자열만 쏙 뽑기!
+                .build();
+    }
 }
